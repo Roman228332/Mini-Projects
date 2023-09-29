b48342083b1bcc808f84ba80f4818d6c76fbbaac
Roman228332 <net.rpotapov2005.com@gmail.com>
2023-09-29 17:44:31 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,27 +1,27 @@
 #6задание
-#n = int(input('Введите число: '))
-#f = 1
-#for i in range(1, n+1):
-    #   f = f * i
-#print(f)
+n = int(input('Введите число: '))
+f = 1
+for i in range(1, n+1):
+    f = f * i
+print(f)
 
 
 
 #2задание
-#a = int(input('Введите первое число: '))
-#b = int(input('Введите второе число: '))
-#if a<b:
-    #for i in range(a, b+1):
-        #print(i)
-#else:
-    #for x in range(a, b-1, -1):
-        #print(x)
+a = int(input('Введите первое число: '))
+b = int(input('Введите второе число: '))
+if a<b:
+    for i in range(a, b+1):
+        print(i)
+else:
+    for x in range(a, b-1, -1):
+        print(x)
 
 
 
 #8задание
-#n = int(input('Введите число: '))
-#for i in range(1, n + 1):
-    #for j in range(1, i + 1):
-        #print(j, sep='', end='')
-    #print()
+n = int(input('Введите число: '))
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, sep='', end='')
+    print()
