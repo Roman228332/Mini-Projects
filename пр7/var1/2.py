4fe8ecb1e3aff77d1a0c74e4fcee6af1c420bfe8
Roman228332 <net.rpotapov2005.com@gmail.com>
2023-11-16 22:40:08 +0300

    zzz

--- пр7/var1/2.py
+++ пр7/var1/2.py
@@ -1,13 +1,15 @@
-import math
-a = [int(n) for n in input().split()]
-b = [int(p) for p in input().split()]
-c = [int(q) for q in input().split()]
-sr_a = sum(a)/len(a)
-sr_b = sum(b)/len(b)
-sr_c = sum(c)/len(c)
-sum_a = sum(a)
-sum_b = sum(b)
-sum_c = sum(c)
-print('Сумма элементов первого массива:', sum_a, '. Среднеарифметическое значение первого массива:', sr_a)
-print('Сумма элементов второго массива:', sum_b, '. Среднеарифметическое значение второго массива:', sr_b)
-print('Сумма элементов третьего массива:', sum_c, '. Среднеарифметическое значение третьего массива:', sr_c)
+def summa(n):
+    return sum(n)
+
+def srzn(m):
+    return (sum(m)/len(m))
+
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+c = list(map(int, input().split()))
+
+
+print('Сумма элементов первого массива:', summa(a), '. Среднеарифметическое значение первого массива:', srzn(a))
+print('Сумма элементов второго массива:', summa(b), '. Среднеарифметическое значение второго массива:', srzn(b))
+print('Сумма элементов третьего массива:', sum(c), '. Среднеарифметическое значение третьего массива:', srzn(c))
